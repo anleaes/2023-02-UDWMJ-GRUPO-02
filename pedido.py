337e660449ab23fe49383d75748edf27a0126b25
nicolas-sa-oliveira <nicolas-sa-oliveira@hotmail.com>
2023-09-29 10:52:21 -0300

    correção pedido

--- pedido.py
+++ pedido.py
@@ -1,5 +1,5 @@
 class pedido:
-    def __init__(self, preco_total, status, cliente)
+    def __init__(self, preco_total, status, cliente):
     self.preco_total = preco_total
     self.status = status
     self.cliente = cliente
