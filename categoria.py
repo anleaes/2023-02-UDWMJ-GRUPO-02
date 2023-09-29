44d396de4aac3f2979ad296f7b5c4e3b69004b67
nicolas-sa-oliveira <nicolas-sa-oliveira@hotmail.com>
2023-09-29 10:51:22 -0300

    correção categoria

--- categoria.py
+++ categoria.py
@@ -1,5 +1,5 @@
 class categoria:
-    def __init__(self, id, nome, descricao)
-    self.id = id
-    self.nome = nome 
-    self.descricao = descricao
+    def __init__(self, id, nome, descricao):
+        self.id = id
+        self.nome = nome 
+        self.descricao = descricao
