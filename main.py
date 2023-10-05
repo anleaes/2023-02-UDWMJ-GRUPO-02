16fc2e121de3e4e6d0d50725cebe3063642684a9
Bryan-Szymczak <bryszymczak@gmail.com>
2023-10-05 11:31:33 -0300

    Atualizei o gitignore e crie as classes na main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from item_pedido import Item_pedido
+from produto import Produto
+
 def main():
-    
+    itp1 = Item_pedido(100, 16.00, 'Erva', 'Erva mate')
+    pr1 = Produto('Erva Mate', 'Erva especializada para chimas', '5/10/23', 'SIM', 'Chás')
+    print(itp1.verificarPedido())
+    print(pr1.verificarData())
 
 if __name__ == "__main__":
     main()
