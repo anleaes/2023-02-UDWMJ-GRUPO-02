4943cc3e5684e43f42e3e447b0d695fec385dd4e
nicolas-sa-oliveira <nicolas-sa-oliveira@hotmail.com>
2023-10-06 10:56:47 -0300

    mudança na main e nas classes cliente e pedido (nome do metodo)

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def main():
     
     C1 = Cliente('Luigi', 'veloso',  'Rua manoel', '51 9999 9999', 'luigi_vava@hotmail.com', 'M')
     pd1 = Pedido(16.00, 'ativo', 'Luigi')
-    print(C1.email())
-    print(pd1.status())
+    print(C1.endemail())
+    print(pd1.situstatus())
 
 if __name__ == "__main__":
     main()
