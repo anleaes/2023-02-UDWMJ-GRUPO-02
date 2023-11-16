c653e1a3ae31b2d2c1a0c31a116645bea683b5ce
Luigi <luigivibias11@gmai.com>
2023-11-16 11:13:14 -0300

    implementei o app client

--- a3app/a3app/settings.py
+++ a3app/a3app/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clients.apps.ClientsConfig',
     'products.apps.ProductsConfig',
     'socialnetworks.apps.SocialnetworksConfig',
     'categories.apps.CategoriesConfig',
