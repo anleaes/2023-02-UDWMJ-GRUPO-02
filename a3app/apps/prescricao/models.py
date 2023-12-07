04bf535571a2c7cd8996e47f57a88f7d9365875b
Luigi <luigivibias11@gmai.com>
2023-12-07 11:12:48 -0300

    atualização nos models de prescrição

--- a3app/apps/prescricao/models.py
+++ a3app/apps/prescricao/models.py
@@ -1,23 +1,19 @@
 from django.db import models
-from medico.models import Medico
 
 # Create your models here.
 class Prescricao(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
-    # STATUS_CHOICES = (
-    #     ('Em andamento', 'Em andamento'),
-    #     ('Finalizado', 'Finalizado'),
-    #     ('Cancelado', 'Cancelado'),
-    # )
-    
-    #status = models.CharField('Status', max_length=20, choices=STATUS_CHOICES, null=True, blank=True, default='Em andamento')
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    medico = models.CharField('Medico', max_length=50)
+    description = models.TextField('Descricao', max_length=100, default='-')
+    medicamento = models.TextField('Medicamentos', max_length=100, default='-')
     
     class Meta:
-        verbose_name = 'Prescrição'
-        verbose_name_plural = 'Prescrições'
-        ordering =['-created_on']
+        verbose_name = 'Prescricao'
+        verbose_name_plural = 'Prescricoes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
         
     
