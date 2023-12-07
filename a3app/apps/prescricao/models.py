1e8546af0d3887bea5ce233afb12c4d0df3d7e40
Luigi <luigivibias11@gmail.com>
2023-12-06 21:38:31 -0300

    Implementei os models de Prescrição

--- a3app/apps/prescricao/models.py
+++ a3app/apps/prescricao/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from medico.models import Medico
 
 # Create your models here.
+class Prescricao(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    # STATUS_CHOICES = (
+    #     ('Em andamento', 'Em andamento'),
+    #     ('Finalizado', 'Finalizado'),
+    #     ('Cancelado', 'Cancelado'),
+    # )
+    
+    #status = models.CharField('Status', max_length=20, choices=STATUS_CHOICES, null=True, blank=True, default='Em andamento')
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'Prescrição'
+        verbose_name_plural = 'Prescrições'
+        ordering =['-created_on']
+        
+    
