d853782c6085258e0c994e1ac442884284203090
Luigi <luigivibias11@gmai.com>
2023-12-07 11:13:10 -0300

    atualizei as urls de prescrição

--- a3app/apps/prescricao/urls.py
+++ a3app/apps/prescricao/urls.py
@@ -5,9 +5,7 @@ app_name = 'prescricao'
 
 urlpatterns = [
     path('', views.list_prescricao, name='list_prescricao'),
-    path('adicionar/<int:id_prescricao>/', views.add_prescricao, name='add_prescricao'),
+    path('adicionar/', views.add_prescricao, name='add_prescricao'),
+    path('editar/<int:id_prescricao>/', views.edit_prescricao, name='edit_prescricao'),
     path('excluir/<int:id_prescricao>/', views.delete_prescricao, name='delete_prescricao'),
-    path('excluir-item/<int:id_prescricao_item>/', views.delete_prescricao_item, name='delete_prescricao_item'),
-    path('adicionar-item/<int:id_prescricao>/', views.add_prescricao_item, name='add_prescricao_item'),
-    path('editar-status/<int:id_prescricao>/', views.edit_prescricao_status, name='edit_prescricao_status'),
 ]
