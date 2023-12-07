3f97a9eeb7f4421f6bcc68c392c9536c676e275f
Luigi <luigivibias11@gmail.com>
2023-12-07 20:48:32 -0300

    Criei o models de exames

--- a3app/apps/exames/models.py
+++ a3app/apps/exames/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Exame(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    first_name = models.TextField('Descrição', max_length=200, default='')
+ 
+    class Meta:
+        verbose_name = 'Exame'
+        verbose_name_plural = 'Exames'
+        ordering =['id']
+
+    def __str__(self):
+        return self.first_name
