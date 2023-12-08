f1323f3cec4dd8770118f080fa3b059204809023
nicolas <nicolas-sa-oliveira@hotmail.com>
2023-12-08 00:41:12 -0300

    implementação-atendimento

--- a3app/apps/atendimento/models.py
+++ a3app/apps/atendimento/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from paciente.models import Paciente
 
 # Create your models here.
+class Atendimento(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    STATUS_CHOICES = (
+        ('Agendado', 'Agendado'),
+        ('Cancelado', 'Cancelado')
+    )
+    status = models.CharField('Status', max_length=20, choices=STATUS_CHOICES, null=True, blank=True, default='Agendado')
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'atendimento'
+        verbose_name_plural = 'atendimentos'
+        ordering =['-created_on']
+
+    def __str__(self):
+        return  self.status
+
+
