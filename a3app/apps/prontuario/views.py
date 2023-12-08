9c24ddc01f42019871fe4559947532581508fc4a
Bryan <bryszymczak@gmail.com>
2023-12-08 03:30:25 -0300

    Criei a views de Prontuario

--- a3app/apps/prontuario/views.py
+++ a3app/apps/prontuario/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ProntuarioForm
+from .models import Prontuario
 
 # Create your views here.
+
+def add_prontuario(request):
+    template_name = 'prontuario/add_prontuario.html'
+    context = {}
+    if request.method == 'POST':
+        form = Prontuario(request.POST, request.FILES)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('prontuario:list_prontuario')
+    form = ProntuarioForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_prontuario(request):
+    template_name = 'prontuario/list_prontuario.html'
+    prontuario = Prontuario.objects.filter()
+    context = {
+        'prontuario': prontuario
+    }
+    return render(request, template_name, context)
+
+def edit_prontuario(request, id_prontuario):
+    template_name = 'prontuario/add_prontuario.html'
+    context ={}
+    prontuario = get_object_or_404(Prontuario, id=id_prontuario)
+    if request.method == 'POST':
+        form = ProntuarioForm(request.POST, request.FILES,  instance=prontuario)
+        if form.is_valid():
+            form.save()
+            return redirect('prontuario:list_prontuario')
+    form = ProntuarioForm(instance=prontuario)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_prontuario(request, id_prontuario):
+    prontuario = Prontuario.objects.get(id=id_prontuario)
+    prontuario.delete()
+    return redirect('prontuario:list_prontuario')
